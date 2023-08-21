e41dafd23718dddb1919c6a06ea579f6d03f3891
David W.H. Swenson <dwhs@hyperblazer.net>
2023-08-21 18:30:13 -0500

    fixes for a few more test cases

--- ticgithub/emailcleaners.py
+++ ticgithub/emailcleaners.py
@@ -2,8 +2,12 @@ import re
 
 # consider https://github.com/shinnn/github-username-regex
 def at_mention_cleaner(content):
-    regex = re.compile("^(?P<pre>[^A-Za-z0-9]?)@(?P<post>[A-Za-z0-9])")
-    content = re.sub(regex, "\g<pre>@<!-- -->\g<post>", content)
+    regex = re.compile("(?P<pre>[^A-Za-z0-9]+)@(?P<post>[A-Za-z0-9])")
+    replacement = r"\g<pre>@<!-- -->\g<post>"
+    if not re.fullmatch(regex, content):
+        content = re.sub(regex, replacement, content)
+    at_starts = re.compile("^(?P<pre>)@(?P<post>[A-Za-z0-9])")
+    content = re.sub(at_starts, replacement, content)
     return content
 
 DEFAULT_CLEANERS = (
--- ticgithub/tests/test_emailcleaners.py
+++ ticgithub/tests/test_emailcleaners.py
@@ -13,7 +13,8 @@ from ticgithub.emailcleaners import *
     ("1@dwhswenson", "1@dwhswenson"),
     ("a@dwhswenson", "a@dwhswenson"),
     ("foo@dwhswenson", "foo@dwhswenson"),
-
+    ("blah!@dwhswenson", "blah!@<!-- -->dwhswenson"),
+    (" @dwhswenson", " @<!-- -->dwhswenson"),
 ])
 def test_at_mention_cleaner(unclean, cleaned):
     assert at_mention_cleaner(unclean) == cleaned
